1b1f6075e06f1c480c087cc435f159e79ed074c8
Esteban Cruz <cruzcr6@gmail.com>
2021-11-18 19:50:38 -0600

    Level 4 - Salt and Hash using bcrypt

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require('md5');
+// Require bcrypt and define the number of salt rounds
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -47,17 +49,18 @@ app.get("/login", function(req, res) {
  */
 app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({ email: username }, function(err, foundUser) {
         if (err) {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    // If it is the correct user
+                // Use bcrypt.compar method. Load hash from your password DB
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    // result == true
                     res.render("secrets");
-                }
+                });
             }
         }
     });
@@ -74,19 +77,24 @@ app.get("/register", function(req, res) {
  * POST Register route
  */
 app.post("/register", function(req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+
+        // Save new user
+        newUser.save(function(err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
     });
 
-    // Save new user
-    newUser.save(function(err) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
-    });
+
 });
 
 app.listen(3000, function() {
