1ec2046e2e1d7d6d363f10cfdd1c85ff77eb3862
Esteban Cruz <cruzcr6@gmail.com>
2021-11-20 19:36:22 -0600

    Level 5 - Cookies and Sessions implemented

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-// Require bcrypt and define the number of salt rounds
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+// 1. Require the packages
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 
 const app = express();
 
@@ -17,6 +19,19 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+// 2. Use express-session, set the properties for the session
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+}));
+
+// 3.1 Initialize the passport
+app.use(passport.initialize());
+// 3.2 Tell the app to use passport to deal with the sessions
+app.use(passport.session());
 
 // Connect to the DB
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
@@ -27,8 +42,19 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
+// 4. Plugin the userSchema to use the passportLocalMongoose to hash and 
+// salt the password and save the user in the MongoDB
+userSchema.plugin(passportLocalMongoose);
+
 // Create a user model
 const User = new mongoose.model("User", userSchema);
+
+// 5. Set up the passport local configuration
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 /**
  * GET Home route
@@ -48,22 +74,30 @@ app.get("/login", function(req, res) {
  * POST Login route
  */
 app.post("/login", function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
-    User.findOne({ email: username }, function(err, foundUser) {
+    // Use the login method from password
+    req.login(user, function(err) {
         if (err) {
             console.log(err);
         } else {
-            if (foundUser) {
-                // Use bcrypt.compar method. Load hash from your password DB
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                    // result == true
-                    res.render("secrets");
-                });
-            }
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            });
         }
     });
+
+});
+
+/**
+ * GET Logout route
+ */
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
 });
 
 /**
@@ -77,24 +111,31 @@ app.get("/register", function(req, res) {
  * POST Register route
  */
 app.post("/register", function(req, res) {
-
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-
-        // Save new user
-        newUser.save(function(err) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("secrets");
-            }
-        });
+    User.register({ username: req.body.username }, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            });
+        }
     });
 
+});
 
+/**
+ * GET Secrets route
+ */
+app.get("/secrets", function(req, res) {
+    console.log("Validating the user in the secrets page");
+
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        console.log("User not authenticated");
+        res.redirect("/login");
+    }
 });
 
 app.listen(3000, function() {
